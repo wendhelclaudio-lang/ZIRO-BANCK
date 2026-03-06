d2b88a4dc67bd14b1321d790a54184895557ef45
wendhelclaudio-lang <wendhelclaudio@gmail.com>
2026-03-06 16:01:04 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,17 @@
-const CACHE_NAME = 'meucaixa-v2';
+const CACHE_NAME = 'meucaixa-v3';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  './icon.svg'
 ];
 
-// Instala o service worker e faz o cache dos arquivos básicos
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-// Atualiza os caches antigos se houver mudança de versão
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys => {
@@ -25,7 +22,6 @@ self.addEventListener('activate', e => {
   );
 });
 
-// Intercepta as requisições (mantém o app abrindo rápido e permite PWA)
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(response => response || fetch(e.request))
